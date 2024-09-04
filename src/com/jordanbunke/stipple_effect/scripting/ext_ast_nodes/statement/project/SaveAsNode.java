33ca6b17cf40e94e7744e840441e1aa8b44ebe2d
Jordan Bunke <jordantb99@gmail.com>
2024-09-04 05:08:54 +0100

    > Disabled saving from `project.save_as(save_config);` in the demo build

--- src/com/jordanbunke/stipple_effect/scripting/ext_ast_nodes/statement/project/SaveAsNode.java
+++ src/com/jordanbunke/stipple_effect/scripting/ext_ast_nodes/statement/project/SaveAsNode.java
@@ -4,8 +4,6 @@ import com.jordanbunke.delta_time.scripting.ast.nodes.expression.ExpressionNode;
 import com.jordanbunke.delta_time.scripting.ast.symbol_table.SymbolTable;
 import com.jordanbunke.delta_time.scripting.util.FuncControlFlow;
 import com.jordanbunke.delta_time.scripting.util.TextPosition;
-import com.jordanbunke.stipple_effect.project.SEContext;
-import com.jordanbunke.stipple_effect.project.SaveConfig;
 import com.jordanbunke.stipple_effect.scripting.ext_ast_nodes.type.SaveConfigTypeNode;
 import com.jordanbunke.stipple_effect.utility.StatusUpdates;
 
@@ -21,19 +19,9 @@ public final class SaveAsNode extends ProjectStatementNode {
 
     @Override
     public FuncControlFlow execute(final SymbolTable symbolTable) {
-        final SEContext project = getProject(symbolTable);
-        final SaveConfig sc = (SaveConfig) arguments.getValues(symbolTable)[0];
-
-        project.setSaveConfig(sc);
-
-        if (sc.hasSaveAssociation()) {
-            project.setSaveConfig(sc);
-            sc.save(project);
-        } else
-            StatusUpdates.scriptActionNotPermitted(
-                    "save the project with this save configuration",
-                    "the folder and name configuration is invalid or incomplete",
-                    arguments.args()[0].getPosition());
+        StatusUpdates.scriptActionNotPermitted("save the project",
+                "the demo does not permit saving",
+                scope.caller().getPosition());
 
         return FuncControlFlow.cont();
     }
